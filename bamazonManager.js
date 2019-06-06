58f7184c9f6bf9bdbd31c783b73dc54c4346ce83
Mike Z <mzapata2991@gmail.com>
2019-06-05 22:10:36 -0500

    got third menu and exit working

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,14 +16,23 @@ function start() {
             name: "menuOptions",
             type: "list",
             message: "Would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "EXIT"]
         })
         .then(function (answer) {
             if (answer.menuOptions === "View Products for Sale") {
                 displayItems();
             }
             else if (answer.menuOptions === "View Low Inventory") {
-               lowInventory(); 
+                lowInventory();
+            }
+            else if (answer.menuOptions === "Add to Inventory") {
+                addInventory();
+            }
+            else if (answer.menuOptions === "Add New Product") {
+                addNewProduct();
+            }
+            else if (answer.menuOptions === "EXIT") {
+                connection.end();
             }
         });
 }
@@ -41,10 +50,11 @@ function displayItems() {
         }
         console.log(displayTable.toString());
         // secondPrompts();
+        start();
     });
 }
 
-function lowInventory(){
+function lowInventory() {
     connection.query("SELECT * FROM products WHERE stock_quantity<=5", function (err, res) {
         if (err) throw err;
         var displayTable = new Table({
@@ -57,8 +67,51 @@ function lowInventory(){
         }
         console.log(displayTable.toString());
         // secondPrompts();
+        start();
     });
 
+}
+
+function addInventory() {
+    inquirer
+        .prompt([
+            {
+                name: "id_of_product",
+                type: "input",
+                message: "What is the ID of the product you would like to add inventory to?",
+            },
+            {
+                name: "item_Quantity",
+                type: "input",
+                message: "How many units of this product would you like to add?",
+            },
+
+        ]).then(function (answer) {
+            let itemID = answer.id_of_product;
+            let itemQuantity = answer.item_Quantity;
+            addedItem(itemID, itemQuantity);
+        });
+
+}
+
+function addedItem(ID, amount) {
+    connection.query('Select * FROM products WHERE item_id = ' + ID, function (err, res) {
+        if (err) {
+            console.log(err)
+        }
+
+        if (amount > 0) {
+            connection.query("UPDATE products SET stock_quantity = stock_quantity + " + amount + " WHERE item_id = " + ID);
+        }
+        // } else if (amount <= res[0].stock_quantity) {
+        //     var totalCost = res[0].price * amount;
+        //     console.log("Your total cost for " + amount + " " + res[0].product_name + "(s) is $" + totalCost + ", Thank you!");
+        //     connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amount + " WHERE item_id = " + ID);
+        // }
+        displayItems();
+    });
+
+    // connection.end();
 }
 
 start();
